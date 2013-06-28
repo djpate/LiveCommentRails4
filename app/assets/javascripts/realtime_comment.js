9521d2f2883206c044ff9c599f606c91426378d9
Christophe Verbinnen <djpate@gmail.com>
2013-06-28 09:15:11 -0700

    Fix js/html to update the comment list.

--- app/assets/javascripts/realtime_comment.js
+++ app/assets/javascripts/realtime_comment.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function() {
+  
+  var Comment = Backbone.Model.extend();
+  var template = _.template($("#comment_template").html());
   var source = new EventSource('/realtime/comments');
+
   source.addEventListener('new', function(e) {
-   console.log(e);
+    data = $.parseJSON($.parseJSON(e.data));
+    var comment = new Comment({title: data.title, content: data.content});
+    var html = template(comment.toJSON());
+    $('#comments_container').append(html);
   });
 });
