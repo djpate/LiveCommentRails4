7f10094bac26ed46742ced73b1d9ee4b148712ac
Christophe Verbinnen <djpate@gmail.com>
2013-06-27 23:27:53 -0700

    required config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ LiveCommentRails4::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.preload_frameworks = true
+  config.allow_concurrency = true
+  config.autoload_paths += Dir["#{config.root}/lib/**/"]
 end
